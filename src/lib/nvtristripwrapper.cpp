d4a12c4fbced2b66f5616b48b1b795f7b84ef456
fo76utils <87907510+fo76utils@users.noreply.github.com>
2025-01-12 20:45:52 +0100

    Removed NvTriStrip, use meshoptimizer instead for stripifying

--- src/lib/nvtristripwrapper.cpp
+++ src/lib/nvtristripwrapper.cpp
@@ -1,50 +1,54 @@
 #include "nvtristripwrapper.h"
 #include "data/niftypes.h"
 
-#include <NvTriStrip.h>
+#include "meshoptimizer/src/meshoptimizer.h"
 
 
 QVector<QVector<quint16> > stripify( QVector<Triangle> triangles, bool stitch )
 {
-	if ( triangles.count() <= 0 )
-		return QVector<QVector<quint16> >();
+	QVector<QVector<quint16>>	strips;
 
-	unsigned short * data = (unsigned short *)malloc( triangles.count() * 3 * sizeof( unsigned short ) );
-	if ( !data )
-		return QVector<QVector<quint16> >();
-
-	for ( int t = 0; t < triangles.count(); t++ ) {
-		data[ t * 3 + 0 ] = triangles[t][0];
-		data[ t * 3 + 1 ] = triangles[t][1];
-		data[ t * 3 + 2 ] = triangles[t][2];
-	}
-
-	PrimitiveGroup * groups  = 0;
-	unsigned short numGroups = 0;
-
-	SetStitchStrips( stitch );
-	//SetCacheSize( 64 );
-	GenerateStrips( data, triangles.count() * 3, &groups, &numGroups );
-	free( data );
-
-	QVector<QVector<quint16> > strips;
-
-	if ( !groups )
+	if ( triangles.isEmpty() )
 		return strips;
 
-	for ( int g = 0; g < numGroups; g++ ) {
-		if ( groups[g].type == PT_STRIP ) {
-			QVector<quint16> strip( groups[g].numIndices, 0 );
+	size_t	numTriangles = size_t( triangles.size() );
+	std::vector< unsigned int >	triangleBuf( numTriangles * 3 );
+	std::vector< unsigned int >	tristripBuf( meshopt_stripifyBound( triangleBuf.size() ) );
 
-			for ( quint32 s = 0; s < groups[g].numIndices; s++ ) {
-				strip[s] = groups[g].indices[s];
-			}
+	quint16	maxVertex = 0;
+	for ( size_t i = 0; i < numTriangles; i++ ) {
+		const Triangle &	t = triangles.at( qsizetype(i) );
+		quint16	v0 = t[0];
+		quint16	v1 = t[1];
+		quint16	v2 = t[2];
+		maxVertex = std::max( maxVertex, std::max( v0, std::max( v1, v2 ) ) );
+		tristripBuf[i * 3] = v0;
+		tristripBuf[i * 3 + 1] = v1;
+		tristripBuf[i * 3 + 2] = v2;
+	}
+	meshopt_optimizeVertexCacheStrip( triangleBuf.data(), tristripBuf.data(), triangleBuf.size(),
+										size_t( maxVertex ) + 1 );
 
-			strips.append( strip );
+	size_t	indicesCnt = meshopt_stripify( tristripBuf.data(), triangleBuf.data(), triangleBuf.size(),
+											size_t( maxVertex ) + 1, (unsigned int) stitch - 1U );
+
+	for ( size_t i = 0; i < indicesCnt; i++ ) {
+		size_t	j = 0;
+		for ( ; ( i + j ) < indicesCnt; j++ ) {
+			if ( tristripBuf[i + j] == (unsigned int) -1 )
+				break;
+		}
+		if ( j > 0 ) {
+			strips.append( QVector<quint16>() );
+			strips.last().resize( qsizetype( j ) );
+			quint16 *	stripData = strips.last().data();
+			do {
+				*stripData = quint16( tristripBuf[i] );
+				i++;
+				stripData++;
+			} while ( --j );
 		}
 	}
-
-	delete [] groups;
 
 	return strips;
 }
